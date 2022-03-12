34518fa2e9bbbb8365363af485546e4bd906b144
chenzican <1048196021@qq.com>
2022-03-12 19:58:15 +0800

    goto_v2: Persistent storage by gob

--- store.go
+++ store.go
@@ -1,14 +1,56 @@
 package main
 
-import "sync"
+import (
+	"encoding/gob"
+	"io"
+	"log"
+	"os"
+	"sync"
+)
 
 type URLStore struct {
 	urls map[string]string
 	mu   sync.RWMutex
+	file *os.File
 }
 
-func NewURLStore() *URLStore {
-	return &URLStore{urls: make(map[string]string)}
+type record struct {
+	Key string
+	URL string
+}
+
+func NewURLStore(filename string) *URLStore {
+	s := &URLStore{urls: make(map[string]string)}
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal("Error opening URLStore:", err)
+	}
+
+	s.file = f
+	if err := s.load(); err != nil {
+		log.Fatal("Error loading URLStore:", err)
+	}
+
+	return s
+}
+
+func (s *URLStore) load() error {
+	if _, err := s.file.Seek(0, 0); err != nil {
+		return err
+	}
+	d := gob.NewDecoder(s.file)
+	var err error
+	for err == nil {
+		var r record
+		if err = d.Decode(&r); err == nil {
+			s.Set(r.Key, r.URL)
+		}
+	}
+
+	if err == io.EOF {
+		return nil
+	}
+	return err
 }
 
 func (s *URLStore) Get(key string) string { // get shortURL from LongURL
@@ -39,9 +81,17 @@ func (s *URLStore) Put(url string) string { // longURL -> get shortURL
 	for {
 		key := genKey(s.Count())
 		if s.Set(key, url) {
+			if err := s.save(key, url); err != nil {
+				log.Fatal("Error saving to URLStore: ", err)
+			}
 			return key
 		}
 	}
 	// shouldn't get shortURL
 	return ""
+}
+
+func (s *URLStore) save(key, url string) error {
+	e := gob.NewEncoder(s.file)
+	return e.Encode(record{key, url})
 }
